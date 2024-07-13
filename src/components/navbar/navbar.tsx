2adeef2cc13372b6ab4639902fceb01b15d1b4ff
Harsh <harshguptaaa17@gmail.com>
2024-07-14 00:20:51 +0530

    initial push

---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,29 @@
+export default function Navbar() {
+    <>
+    <div>
+    <ul className="md:flex lg:gap-10 gap-6 text-white">
+          <button className={`hover:text-[#3cffd0]`}>
+           reddit
+          </button>
+        </ul>
+        <form
+          className={`md:flex bg-[#292929] rounded-lg hidden px-2 items-center group`}
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+        >
+          {/* <Search
+            className={`hover:text-[#3cffd0] text-white cursor-pointer ${isSearchResultsPage ? "hover:text-[#a980ff]" : ""}`}
+            size={16}
+            onClick={handleClick}
+          /> */}
+          <input
+            // ref={searchInputRef}
+            type="text"
+            className={` h-7 w-60 lg:w-96 placeholder:text-sm font-franklin placeholder:font-franklin placeholder:text-center bg-[#292929] text-white outline-none px-4 text-sm`}
+            placeholder="Search"
+          />
+        </form>
+    </div>
+    </>
+}
