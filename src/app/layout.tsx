ff31e0c6ab677212d8c4d7df7cd3f83f8476ba10
Javed Ansari <javedans2003@gmail.com>
2024-07-14 00:32:59 +0530

    nav and side position

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import SideBar from "@/components/sidebar/sidebar";
+import Navbar from "@/components/navbar/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +19,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <SideBar />
-        {children}
+        <div>
+          <Navbar />
+          <div>
+            <SideBar />
+            {children}
+          </div>
+        </div>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,5 @@ import Navbar from "@/components/navbar/navbar";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <>
-      <Navbar />
-      <div className="flex flex-col items-center justify-center h-screen">
-        <Image src="/logo.svg" alt="logo" width={200} height={200} />
-      </div>
-    </>
-  );
+  return <></>;
 }
